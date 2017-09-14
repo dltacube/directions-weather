74441adbe40c34b7cc508b8b473c0daa14a20ab4
[heyoni] <[jrevah@gmail.com]>
2017-09-14 17:12:36 -0400

    test

--- js/weather.js
+++ js/weather.js
@@ -17,10 +17,29 @@ var getWeatherDarkSyk = function () {
 };
 
 var getWeatherOpenWeatherMap = function () {
-    console.log("fetching weather from open weather map");
-    var key = '9648c16ab42255fec8a51db86277e4d7';
-    var baseUrl = 'api.openweathermap.org/data/2.5/weather/';
+    var invocation = new XMLHttpRequest()
+    var url = 'https://api.darksky.net/forecast/a214b166daec400f159accdddb580373/41.87811000000001,-87.62979000000001';
 
-    var firstPoint = weatherCoords[0];
-    console.log(baseUrl + 'lat=' + firstPoint.lat + '&lon=' + firstPoint.lng + '&APPID=' + key);
+    function handler(data) {
+        console.log(data)
+    };
+
+    function callOtherDomain() {
+        if (invocation) {
+            invocation.open('GET', url, true);
+            invocation.onreadystatechange = handler;
+            invocation.send();
+        }
+    };
+
+
 }
+
+// var getWeatherOpenWeatherMap = function () {
+//     console.log("fetching weather from open weather map");
+//     var key = '9648c16ab42255fec8a51db86277e4d7';
+//     var baseUrl = 'api.openweathermap.org/data/2.5/weather/';
+//
+//     var firstPoint = weatherCoords[0];
+//     console.log(baseUrl + 'lat=' + firstPoint.lat + '&lon=' + firstPoint.lng + '&APPID=' + key);
+// }
