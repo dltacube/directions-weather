fe5c444a4df539fcf95a1a41c1497098b8b52ac2
Jonathan Revah <jonathanrevah@Jonathans-MBP.fios-router.home>
2017-10-05 16:56:39 -0400

    we have icons

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ currentRoute = {
 var app = function () {
     var self = this;
 
-    // self.dsWeather = ko.observableArray([{
-    //     lat: map.center.lat(),
-    //     lng: map.center.lng()
-    // }]);
     self.markers = [];
     self.route = ko.observable();
     self.waypoints = ko.observableArray();
@@ -109,13 +105,8 @@ var getWeatherDarkSky = function (viewModel) {
 
     for (var i = 0; i < viewModel.waypoints().length; i++) {
         var waypoint = viewModel.waypoints()[i];
-        // another way of doing this would be to define a separate function and call that for the ajax request
         (function (waypoint) {
-            // waypoint = viewModel.waypoints()[i];
             var qUrl = baseUrl + waypoint.lat + ',' + waypoint.lng;
-            console.log(qUrl);
-
-            var result;
             $.ajax(qUrl, {
                 dataType: 'jsonp',
                 success: function (data) {
@@ -127,7 +118,7 @@ var getWeatherDarkSky = function (viewModel) {
 };
 
 var attachWeatherSummary = function (result, waypoint) {
-    // viewModel.markers[i].marker.setVisible(false);
+    // result sample: https://api.darksky.net/forecast/a214b166daec400f159accdddb580373/37.8267,-122.4233
     myLatLng = {
         lat: waypoint.lat,
         lng: waypoint.lng
@@ -136,22 +127,33 @@ var attachWeatherSummary = function (result, waypoint) {
     for (marker in viewModel.markers) {
         var point = viewModel.markers[marker];
         if (point.marker.position.equals(wpts)) {
-         point.infoWindow.setContent(result.currently.summary);
+            var hourly = result.hourly.data;
+            var icon = hourly[0].icon;
+            var content = hourly[0].summary + '<br>' +
+                hourly[0].icon + '<br>' +
+                '<canvas id="waypoint' + marker + '" width="64" height="64">' + icon + '</canvas><br>' +
+                'precip intensity: ' + hourly[0].precipIntensity + '<br>' +
+                'precip chance: ' + hourly[0].precipProbability + '<br>' +
+                'temperature: ' + hourly[0].temperature + '<br>' +
+                'wind speed: ' + hourly[0].windSpeed+ '<br>' +
+                'wind gust: ' + hourly[0].windGust + '<br>' +
+                'visibility: ' + hourly[0].visibility+ '<br>';
+         point.infoWindow.setContent(content);
          point.infoWindow.open(map, point.marker);
+         generateIcon('waypoint' + marker, icon);
         }
     }
-    // var infoWindow = new google.maps.InfoWindow({
-    //     map: map,
-    //     content: result.currently.summary,
-    //     position: myLatLng
-    // });
-    // viewModel.markers[i].infoWindow = infoWindow;
-    // var marker = viewModel.markers[i].marker = new google.maps.Marker;
-    // marker.setPosition(myLatLng);
-    // marker.addListener('click', function (infoWindow, marker) {
-    //     infoWindow.open(map, marker);
-    // })
-}
+};
+
+var skycons = new Skycons();
+var generateIcon = function (tag, icon) {
+    var re = new RegExp('-', 'g');
+    console.log(icon);
+    var icon = icon.replace(re, '_').toUpperCase();
+    console.log(icon);
+    skycons.add(document.getElementById(tag), Skycons[icon]);
+    skycons.play();
+};
 
 var getWeatherOpenWeatherMap = function () {
     var invocation = new XMLHttpRequest()
